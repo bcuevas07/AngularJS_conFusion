7a16ecf8f640a7440bb61bd96261c3dd298031f1
bcuevas07 <bcuevas07@yahoo.com>
2016-06-06 00:28:24 -0700

    Completed Client-Server Communication using $http exercise

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,13 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes= [];
+            menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes=response.data;
+                }
+            );
 
                         
             $scope.select = function(setTab) {
@@ -69,10 +75,16 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             
-            $scope.dish = dish;
+            $scope.dish = {};
+            
+            menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response) {
+                    $scope.dish=response.data;
+                    $scope.showDish = true;
+                }
+            );
             
         }])
 
@@ -96,7 +108,14 @@ angular.module('confusionApp')
         // implement the IndexController and About Controller here
         .controller('IndexController', [ '$scope', '$stateParams', 'menuFactory', 'corporateFactory', function($scope, $stateParams, menuFactory, corporateFactory) {
         
-            $scope.featured_dish = menuFactory.getDish(0);
+            $scope.featured_dish = {};
+            menuFactory.getDish(0)
+            .then(
+                function(response) {
+                    $scope.featured_dish = response.data;
+                    $scope.showDish = true;
+                }
+            );
             
             $scope.promotion = menuFactory.getPromotion(0);
             
